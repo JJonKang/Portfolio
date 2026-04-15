c0f17a8c8c278f0c1f6fefdb1b76a177b6c54cd7
JJonKang <jjonkang03@gmail.com>
2026-04-15 15:43:20 -0500

    Added an About.jsx file to introduce myself, as well as more css changes and website alterations

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,17 +1,19 @@
 import './index.css'
 import { useState } from 'react'
+import About from './written/About.jsx'
 
 function App(){
-  const [dark, setDark] = useState(true)
+  const [dark, setDark] = useState(false)
 
   document.body.className = dark ? 'dark' : 'light'
 
   return (
     <div>
       <button onClick={() => setDark(!dark)}>Toggle Theme</button>
-      <h1>Portfolio</h1>
+      <h1>Portfolio (work in progress)</h1>
       <div className="line" />
-      <p>Some stuff</p>
+      <About />
+      <p className="footer-text">Version 0.1.0</p>
     </div>
   )
 }
