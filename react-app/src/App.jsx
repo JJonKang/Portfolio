2c94a75d178afea871a9efd37f71c3792362bb0c
JJonKang <jjonkang03@gmail.com>
2026-04-15 21:04:30 -0500

    More Button Changes

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,9 +4,8 @@ import About from './written/About.jsx'
 
 function App(){
   const [dark, setDark] = useState(false)
-
-  document.body.className = dark ? 'dark' : 'light'
-
+  const btnClass = `box-btn ${dark ? 'box-btn-dark' : 'box-btn-light'}`
+  document.body.className = dark ? 'dark' : 'light';
   return (
     <div>
       <div className="line-u" />
@@ -14,10 +13,10 @@ function App(){
       <h1 className="title">Portfolio (work in progress)</h1>
       <About />
       <div className="box-buttons">
-        <button className="box-btn">Projects</button>
-        <button className="box-btn">Skills</button>
-        <button className="box-btn">Contact</button>
-        <button className="box-btn">Arts</button>
+        <button className={btnClass}>Projects</button>
+        <button className={btnClass}>Skills</button>
+        <button className={btnClass}>Contact</button>
+        <button className={btnClass}>Arts</button>
       </div>
       <p className="footer-text-explore">Home</p>
       <p className="footer-text-version">Version 0.1.0</p>
