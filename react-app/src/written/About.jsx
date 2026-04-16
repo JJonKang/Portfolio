e007f598359293b57bdc75012f1ac442b2ee1f85
JJonKang <jjonkang03@gmail.com>
2026-04-16 14:37:09 -0500

    1: Initialized setup on routing. 2: Refactored space indentation to 2 on all files. 3: Small bug fixes

--- react-app/src/written/About.jsx
+++ react-app/src/written/About.jsx
@@ -1,19 +1,19 @@
 import '../index.css'
 
 function About() {
-    return (
-        <div>
-            <h2>About Me</h2>
-            <p>
-                Hello! My name is Jonathan Kang, and I recently graduated from the
-                University of Illinois at Chicago with a Bachelor's in Computer Science.
-                My focus has been on backend development, primarily in Python,
-                though I've also worked with C, C++, Java, and frontend technologies like JavaScript and TypeScript.
-                Throughout my time at UIC, I’ve been a part of several projects, both as a developer and as a lead,
-                that challenged me to grow technically and as a collaborator.
-            </p>
-        </div>
-    );
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>
+        Hello! My name is Jonathan Kang, and I recently graduated from the
+        University of Illinois at Chicago with a Bachelor's in Computer Science.
+        My focus has been on backend development, primarily in Python,
+        though I've also worked with C, C++, Java, and frontend technologies like JavaScript and TypeScript.
+        Throughout my time at UIC, I’ve been a part of several projects, both as a developer and as a lead,
+        that challenged me to grow technically and as a collaborator.
+      </p>
+    </div>
+  );
 };
 
 export default About
