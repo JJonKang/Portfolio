264b8bc02e0e2b4dcb78e37c3d83bd5dfcf2275a
JJonKang <jjonkang03@gmail.com>
2026-04-25 17:57:04 -0500

    1) Worked on Projects.jsx 2) Small aesthetic adjustments 3) Fixed a trivial svg issue

--- react-app/src/pages/Projects.jsx
+++ react-app/src/pages/Projects.jsx
@@ -1,6 +1,13 @@
 function Projects() {
   return (
-    <div></div>
+    <div>
+      <h1 style={{ marginTop: '35px' }}>Projects</h1>
+      <h3>Data Interaction and Utilization</h3>
+      
+      <h3>Computer Graphics</h3>
+      <h3>Games</h3>
+      <p>If you're curious about any other projects I publically post, check out my GitHub (or other links) on the Contacts page!</p>
+    </div>
   )
 }
 
