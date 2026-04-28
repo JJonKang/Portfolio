4cfa5600c642f2b5fb533fd7e2e18f3c005bb008
JJonKang <jjonkang03@gmail.com>
2026-04-28 15:05:25 -0500

    Worked on adding a certificate logo

--- react-app/src/pages/Certificates.jsx
+++ react-app/src/pages/Certificates.jsx
@@ -1,10 +1,15 @@
+import awsLogo from "@/assets/misc/aws.png"
 function Certificates() {
   return (
     <div>
       <h1 style={{ marginTop: '35px' }}>Certificates</h1>
-      <h2>
-        Amazon Web Services Cloud Practitioner
-      </h2>
+      <div style={{ display: 'flex', alignItems: 'center', gap: '0.75rem' }}>
+        <img src={awsLogo} alt="AWS Logo" className="small-img" />
+        <h2>
+          Amazon Web Services Cloud Practitioner
+        </h2>
+      </div>
+      
     </div>
   )
 }
